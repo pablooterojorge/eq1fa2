3fb18776efb5b1cc93b5f419dbe3a452849a3687
pablooterojorge <94971548+pablooterojorge@users.noreply.github.com>
2022-01-11 17:35:13 +0100

    Update ret_vs_rms.py

--- spintools/ret_vs_rms.py
+++ spintools/ret_vs_rms.py
@@ -10,11 +10,4 @@ def ret_vs_vrms(ii0index, ii0, index2vrms = 20):
             ret[i] = 1 - np.arcsin(np.sqrt(ii0[i]))/np.pi
         else:
             ret[i] = np.arcsin(np.sqrt(ii0[i]))/np.pi
-    fig, (ax1, ax2) = plt.subplots(1,2, sharey=False, figsize=(12,5)) 
-    ax1.set_xlabel('$\Delta V_{rms}$ [V]')
-    ax1.set_ylabel('d $ \Delta n $ / $\lambda $')
-    ax1.plot(vrms, ret, '*k')
-    ax2.set_xlabel('d $ \Delta n $ / $\lambda $')
-    ax2.set_ylabel(r'I/$ I_0 $')
-    ax2.plot(ret, ii0, 'ok')
     return vrms, ret
