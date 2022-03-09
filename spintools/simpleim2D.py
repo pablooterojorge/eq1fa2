ed4e16d927df5c63020d7707bc5115ae06435f91
pablooterojorge <94971548+pablooterojorge@users.noreply.github.com>
2022-03-09 15:39:12 +0100

    Add files via upload

---
+++ spintools/simpleim2D.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+def simpleim2D(img):
+    
+    import numpy as np
+    
+    #simple border search
+    #get center slice as array
+    slice_center = img[:,int(np.size((img,1))/2),1]
+    y3 = 0
+    y4 = 0
+    
+    #compute histogram and get most common value, background brightness
+    hist = np.histogram(slice_center,255,[0, 255])[0]
+    bg_ii0 = np.argmax(hist)
+    
+    if bg_ii0 == 0:
+        bg_ii0 = 1
+        
+    #compute change array
+    change = np.empty(len(slice_center))
+    
+    for i in range(len(change)):
+        change[i] = np.abs(slice_center[i]-bg_ii0)/(255)
+        
+    #compute most common change value after bg, this should correlate to fiber region
+    histchange = np.histogram(change,100,[0, 1])[0]  
+    histchange[0] = 0
+    fb_change = np.argmax(histchange)/100
+    
+    #top down search for fiber edge
+    y3 = np.argmax(change > (33/100*fb_change))
+    #flip array and do bottom up search for fiber edge
+    change = np.flip(change)
+    y4 = len(change) - np.argmax(change > (33/100*fb_change))
+   
+    return y3, y4
+
